a01e570d08bceb2e15912fd28761e48e6b38e722
ioNeXd <ioneworkx@gmail.com>
2026-09-24 18:20:04 -0300

    fix: use primary fingerprint field from VALIDSIG

--- src/modules/autoupdate/SignatureUtils.ts
+++ src/modules/autoupdate/SignatureUtils.ts
@@ -124,7 +124,7 @@ export function interpretGpgStatusOutput(output: string): SignatureCheckOutcome 
 				sawNoPubkey = true;
 				break;
 			case "VALIDSIG":
-				if (parts[7]) fingerprint = parts[7];
+				if (parts[10]) fingerprint = parts[10];
 				else if (parts[1]) fingerprint = parts[1];
 				break;
 		}
